455b8282be64351858704891ebb6b62ffb8b453b
mshinkle <mshinkle@nevada.unr.edu>
2023-04-11 13:32:45 -0700

    feat: update conda setup to run as bash

---
+++ anaconda_setup.sh
@@ -0,0 +1,6 @@
+# mamba create -n DorsalNet_FC python=3.10 -y
+# conda activate DorsalNet_FC
+mamba install -c anaconda libopenblas -y
+mamba install -c "nvidia/label/cuda-11.7.0" cuda-toolkit -y
+mamba install pytorch torchvision sympy ipykernel ipywidgets tqdm matplotlib -y
+mamba install -c conda-forge wandb -y
