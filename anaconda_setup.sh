8c1ae5bed330ebe631c8f78db72fb9b72c9dc795
mshinkle <mshinkle@nevada.unr.edu>
2023-04-27 15:56:01 -0700

    feat: add layer fitting script

--- anaconda_setup.sh
+++ anaconda_setup.sh
@@ -1,5 +1,7 @@
+### Do in normal terminal:
 # mamba create -n DorsalNet_FC python=3.10 -y
 # conda activate DorsalNet_FC
+
 mamba install -c anaconda libopenblas -y
 mamba install -c "nvidia/label/cuda-11.7.0" cuda-toolkit -y
 mamba install pytorch torchvision sympy ipykernel ipywidgets tqdm matplotlib -y
---
+++ code/layer_fitting.py
@@ -0,0 +1,213 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision import transforms as tf
+import sys
+from dorsalnet import DorsalNet, FC, interpolate_frames
+sys.path.append('../code')
+from VWAM.utils import SingleImageFolder, iterate_children, hook_model
+from tqdm import tqdm
+import os
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import wandb
+wandb.login()
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-model", help="model name", default='DorsalNet')
+parser.add_argument("-experiment", help="experiment name", default='NaturalMovies')
+parser.add_argument("-subject", help="subject id", default='S00')
+parser.add_argument("-opt", help="optimizer", default='adam')
+parser.add_argument("-epochs", help="number of epochs", default=50)
+parser.add_argument("-lr", help="learning rate", default=1e-1)
+parser.add_argument("-gpu", help="cuda device", default='0')
+parser.add_argument("-dtype", help="data type", default='bfloat16')
+args = parser.parse_args()
+
+MODEL_NAME = args.model
+OPTIMIZER = args.opt
+N_EPOCHS = args.epochs
+LR_INIT = args.lr
+EXPERIMENT = args.experiment
+SUBJECT_ID = args.subject
+DEVICE = f'cuda:{args.gpu}'
+DTYPE = args.dtype
+
+run = wandb.init(
+    # Set the project where this run will be logged
+    project="DorsalNet_FC_Pilot",
+    # Track hyperparameters and run metadata
+    config={
+        "model_name": MODEL_NAME,
+        "experiment": EXPERIMENT,
+        "subject_id": SUBJECT_ID,
+        "optimizer": OPTIMIZER,
+        "epochs": N_EPOCHS,
+        "learning_rate": LR_INIT,
+})
+
+DTYPE = torch.bfloat16
+
+if MODEL_NAME == 'DorsalNet':
+    model = DorsalNet(False, 32).eval().to(DEVICE).to(DTYPE)
+    model.load_state_dict(torch.load('/home/matthew/Data/DorsalNet_FC/base_models/DorsalNet/pretrained.pth'))
+elif MODEL_NAME == 'alexnet':
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True).eval().to(DEVICE).to(DTYPE)
+elif MODEL_NAME == 'inception_v3':
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True).eval().to(DEVICE).to(DTYPE)
+### Choose downsampling
+MAX_FS = 5000
+DEPTH = 1
+input_size = (1, 3, 32, 112, 112)
+
+def choose_downsampling(activations, max_fs):
+    num_channels = activations.shape[1]
+    if activations.ndim == 4:
+        max_output_dim = int((max_fs / num_channels)**(1/2))
+        return torch.nn.AdaptiveMaxPool2d(max_output_dim)
+    elif activations.ndim == 5:
+        max_output_dim = int((max_fs / num_channels)**(1/3))
+        return torch.nn.AdaptiveMaxPool3d(max_output_dim)
+
+layers_dict = iterate_children(model, depth=DEPTH)
+layers_dict = {layer_name: ds_function for layer_name, ds_function in layers_dict.items() if 'dropout' not in layer_name and 'concat' not in layer_name}
+model = hook_model(model, layers_dict)
+model(torch.randn(input_size).to(DEVICE).to(DTYPE))
+
+layer_downsampling_fns = {}
+for layer_name, layer_activations in model.activations.items():
+    layer_activations = layer_activations
+    # print('**************')
+    # print(layer_name)
+    # print('old_shape:', layer_activations.shape)
+    # print('old # activations:', layer_activations.flatten().shape)
+    layer_downsampling_fn = choose_downsampling(layer_activations, MAX_FS)
+    layer_downsampling_fns[layer_name] = layer_downsampling_fn
+    if layer_downsampling_fn is not None:
+        layer_activations = layer_downsampling_fns[layer_name](layer_activations)
+    # print('new_shape:', layer_activations.shape)
+    # print('new # activations:', layer_activations.flatten().shape)
+### Initialize FC Layer
+trn_brain = np.load(f'/home/matthew/Data/DorsalNet_FC/fMRI_data/{SUBJECT_ID}/NaturalMovies/trn.npy')
+trn_brain = torch.tensor(np.nan_to_num(trn_brain), device=DEVICE)
+n_voxels = trn_brain.shape[1]
+
+val_brain = np.load(f'/home/matthew/Data/DorsalNet_FC/fMRI_data/{SUBJECT_ID}/NaturalMovies/val_rpts.npy')
+val_brain = torch.tensor(np.nan_to_num(val_brain).mean(0), device=DEVICE)
+
+fc = FC(n_voxels).to(DEVICE).to(DTYPE)
+# print(fc)
+
+def column_corr(A, B, dof=0):
+    """Efficiently compute correlations between columns of two matrices
+    
+    Does NOT compute full correlation matrix btw `A` and `B`; returns a 
+    vector of correlation coefficients. FKA ccMatrix."""
+    zs = lambda x: (x-np.nanmean(x, axis=0))/np.nanstd(x, axis=0, ddof=dof)
+    rTmp = np.nansum(zs(A)*zs(B), axis=0)
+    n = A.shape[0]
+    # make sure not to count nans
+    nNaN = np.sum(np.logical_or(np.isnan(zs(A)), np.isnan(zs(B))), 0)
+    n = n - nNaN
+    r = rTmp/n
+    return r
+
+
+batch_sizes = {
+    'NaturalMovies': 30,
+    'vedb_ver01': 50,
+}
+
+preprocess = tf.Compose([
+    tf.Resize(112),
+    tf.ToTensor(),
+])
+
+image_augmentations = tf.Compose([
+    tf.RandomCrop(112, padding=4),
+    tf.RandomRotation(10),
+    tf.RandomCrop(112, padding=3),
+])
+
+trn_dl = DataLoader(
+    SingleImageFolder(f'/home/matthew/Data/DorsalNet_FC/stimuli/{EXPERIMENT}/images/trn', transform=preprocess),
+    batch_size=batch_sizes[EXPERIMENT], 
+    shuffle=False)
+
+val_dl = DataLoader(
+    SingleImageFolder(f'/home/matthew/Data/DorsalNet_FC/stimuli/{EXPERIMENT}/images/val', transform=preprocess),
+    batch_size=batch_sizes[EXPERIMENT], 
+    shuffle=False)
+
+torch.cuda.empty_cache()
+if OPTIMIZER == 'SGD':
+    optimizer = torch.optim.SGD(fc.parameters(), lr=LR_INIT)
+elif OPTIMIZER == 'Adam':
+    optimizer = torch.optim.Adam(fc.parameters(), lr=LR_INIT)
+
+def train():
+    pbar = tqdm(enumerate(trn_dl), total=len(trn_brain), desc=f"Epoch {epoch} Training")
+    trn_epoch_losses = []
+    for i, batch in pbar:
+        optimizer.zero_grad()
+        batch = interpolate_frames(batch, input_size[2])
+        model.forward(image_augmentations(batch).unsqueeze(0).to(DTYPE).to(DEVICE))
+        all_activations = []
+        for layer_name, layer_activations in model.activations.items():
+            layer_downsampling_fn = layer_downsampling_fns[layer_name]
+            if layer_downsampling_fn is not None:
+                layer_activations = layer_downsampling_fn(layer_activations)
+            all_activations.append(layer_activations.mean(0).flatten())
+            model.activations[layer_name] = 0
+        fc_out = fc(torch.cat(all_activations).unsqueeze(0))
+        batch_brain = (trn_brain[min(i+2, len(trn_brain)-1)] + trn_brain[min(i+3, len(trn_brain)-1)]) / 2
+        loss = torch.square(fc_out[0]/1000 - batch_brain).sum().sqrt()
+        loss.backward()
+        optimizer.step()
+        trn_epoch_losses.append(loss.item())
+        pbar.set_postfix_str(f"Mean Epoch Loss: {torch.mean(torch.tensor(trn_epoch_losses)).item():.2f}")
+    return trn_epoch_losses
+
+def validate():
+    with torch.no_grad():
+        pbar = tqdm(enumerate(val_dl), total=len(val_brain), desc=f"Epoch {epoch} Validation")
+        val_outputs = []
+        val_epoch_losses = []
+        for i, batch in pbar:
+            batch = interpolate_frames(batch, input_size[2])
+            model.forward(batch.unsqueeze(0).to(DTYPE).to(DEVICE))
+            all_activations = []
+            for layer_name, layer_activations in model.activations.items():
+                layer_downsampling_fn = layer_downsampling_fns[layer_name]
+                if layer_downsampling_fn is not None:
+                    layer_activations = layer_downsampling_fn(layer_activations)
+                all_activations.append(layer_activations.mean(0).flatten())
+                model.activations[layer_name] = 0
+            fc_out = fc(torch.cat(all_activations).unsqueeze(0))
+            batch_brain = (val_brain[min(i+2, len(val_brain)-1)] + val_brain[min(i+3, len(val_brain)-1)]) / 2
+            loss = torch.square(fc_out[0]/1000 - batch_brain).sum().sqrt()
+            val_outputs.append(fc_out.cpu().float().numpy())
+            val_epoch_losses.append(loss.item())
+            pbar.set_postfix_str(f"Mean Epoch Loss: {torch.mean(torch.tensor(val_epoch_losses)).item():.2f}")
+        ccs = column_corr(np.concatenate(val_outputs), val_brain.cpu().numpy())
+        print(f"Mean Prediction Accuracy: {ccs.mean():.2f}")
+    return val_epoch_losses, ccs
+        
+def log(epoch, trn_epoch_losses, val_epoch_losses, ccs):
+    wandb.log({
+        "epoch": epoch,
+        "trn_loss": torch.mean(torch.tensor(trn_epoch_losses)).item(),
+        "val_loss": torch.mean(torch.tensor(val_epoch_losses)).item(),
+        "val_acc": ccs.mean(),
+    })
+
+save_dir = f'/home/matthew/Data/DorsalNet_FC/fits/{EXPERIMENT}/{SUBJECT_ID}'
+os.makedirs(save_dir, exist_ok=True)
+# epoch = -1
+# validate()
+for epoch in range(N_EPOCHS):
+    trn_epoch_losses = train()
+    val_epoch_losses, ccs = validate()
+    log(epoch, trn_epoch_losses, val_epoch_losses, ccs)
+torch.save(model.state_dict(), f"{save_dir}/{MODEL_NAME}_{OPTIMIZER}_{N_EPOCHS}_{LR_INIT}.pt")
